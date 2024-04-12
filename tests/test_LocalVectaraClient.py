cb6165b1b7a7c067a658e2d2b7fba3a1aebd6b2b
isayhc <isayahculbertson@gmail.com>
2024-04-12 14:22:12 -0400

    created simple test for new derived class

--- tests/test_LocalVectaraClient.py
+++ tests/test_LocalVectaraClient.py
@@ -1,7 +1,8 @@
+# tests/test_LocalVectaraClient.py
 import pytest
 from vectara_cli.core import LocalVectaraClient
 
-def is_LocalVectaraClient_valid():
-    assert LocalVectaraClient().api_key is not None and LocalVectaraClient().api_key  != ""
-    assert LocalVectaraClient().customer_id is not None and LocalVectaraClient().customer_id  != ""
-
+@pytest.mark.parametrize("attribute", ["api_key", "customer_id"])
+def test_LocalVectaraClient_attributes(attribute):
+    client = LocalVectaraClient()
+    assert getattr(client, attribute) is not None and getattr(client, attribute) != ""

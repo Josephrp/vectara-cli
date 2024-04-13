e5e0b1c3145ae80828042b3b5dd942abe43caf09
isayhc <isayahculbertson@gmail.com>
2024-04-13 10:20:27 -0400

    changed cli to argparse

--- vectara_cli/commands/span_text.py
+++ vectara_cli/commands/span_text.py
@@ -1,27 +1,31 @@
 # ./commands/span_text.py
 
-from vectara_cli.rebel_span.noncommercial.nerdspan import Span
-from vectara_cli.utils.config_manager import ConfigManager
-from vectara_cli.core import VectaraClient
-
+import argparse
 import json
 
+from vectara_cli.rebel_span.noncommercial.nerdspan import Span
+from vectara_cli.utils.config_manager import ConfigManager
+from vectara_cli.core import VectaraClient, LocalVectaraClient
+from vectara_cli.rebel_span.noncommercial.nerdspan import Span
 
-def main(args, vectara_client:VectaraClient):
+
+def main():
+    parser = argparse.ArgumentParser(description='Process text using Vectara CLI')
+    parser.add_argument('text', type=str, help='The text to analyze')
+    parser.add_argument('model_name', type=str, help='The name of the model to use')
+    parser.add_argument('model_type', type=str, help='The type of the model')
+    args = parser.parse_args()
     
-    if len(args) < 3:
-        print("Usage: vectara-cli span-text text model_name model_type")
-        return
-    # _, text, model_name, model_type = args
-
-    text = args[0]
-    model_name = args[1]
-    model_type = args[2]
+    try:
+        vectara_client = ConfigManager()
+    except:
+        vectara_client = LocalVectaraClient()
+    
 
     try:
-        span = Span(text, vectara_client, model_name, model_type)
-        span.load_model(model_name, model_type)  
-        output_str, key_value_pairs = span.analyze_text(model_name, model_type)
+        span = Span(args.text, vectara_client, args.model_name, args.model_type)
+        span.load_model(args.model_name, args.model_type)  
+        output_str, key_value_pairs = span.analyze_text(args.model_name, args.model_type)
         print(output_str)
         print(json.dumps(key_value_pairs))
     except Exception as e:
@@ -29,6 +33,4 @@ def main(args, vectara_client:VectaraClient):
 
 
 if __name__ == "__main__":
-    import sys
-
-    main(sys.argv)
+    main()

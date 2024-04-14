e661e2d8d504ace4a2332e076b74440812a6aa4e
isayhc <isayahculbertson@gmail.com>
2024-04-14 11:35:51 -0400

    added some default arguments

--- vectara_cli/commands/span_text.py
+++ vectara_cli/commands/span_text.py
@@ -11,9 +11,9 @@ from vectara_cli.rebel_span.noncommercial.nerdspan import Span
 
 def main():
     parser = argparse.ArgumentParser(description='Process text using Vectara CLI')
-    parser.add_argument('text', type=str, help='The text to analyze')
-    parser.add_argument('model_name', type=str, help='The name of the model to use')
-    parser.add_argument('model_type', type=str, help='The type of the model')
+    parser.add_argument('--text', type=str, default="the lazy fox jumped over the brown dog", help='The text to analyze')
+    parser.add_argument('--model_name', type=str, default='fewnerdsuperfine', help='The name of the model to use')
+    parser.add_argument('--model_type', type=str, default='span_marker', help='The type of the model')
     args = parser.parse_args()
     
     try:
@@ -24,12 +24,14 @@ def main():
 
     try:
         span = Span(args.text, vectara_client, args.model_name, args.model_type)
-        span.load_model(args.model_name, args.model_type)  
-        output_str, key_value_pairs = span.analyze_text(args.model_name, args.model_type)
+        span.load_model()  
+        output_str, key_value_pairs = span.analyze_text()
         print(output_str)
         print(json.dumps(key_value_pairs))
     except Exception as e:
         print("Error processing text:", str(e))
+        
+
 
 
 if __name__ == "__main__":
